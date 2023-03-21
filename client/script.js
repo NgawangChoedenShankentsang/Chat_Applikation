6f9eed350be2e9e672323f50abdbb9ceed6a0b53
NgawangChoedenShankentsang <ngawangchoeden@icloud.com>
2023-03-21 08:52:28 +0100

    MariaDB configuration

--- client/script.js
+++ client/script.js
@@ -1,18 +1,28 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = new WebSocket('ws://localhost:8080');
 
-socket.addEventListener("open", (event) => {
-  console.log("WebSocket connected!");
-  socket.send("Hello, server!");
+// Handle connection opened
+socket.addEventListener('open', (event) => {
+  console.log('WebSocket connection opened');
 });
 
-socket.addEventListener("message", (event) => {
-  console.log(`Received message: ${event.data}`);
+// Handle connection closed
+socket.addEventListener('close', (event) => {
+  console.log('WebSocket connection closed');
 });
 
-socket.addEventListener("close", (event) => {
-  console.log("WebSocket closed.");
+// Handle messages received from server
+socket.addEventListener('message', (event) => {
+  const message = event.data;
+  // Add message to chat display
 });
 
-socket.addEventListener("error", (event) => {
-  console.error("WebSocket error:", event);
+// Handle send button click event
+const sendBtn = document.querySelector('.message-input button');
+const messageInput = document.querySelector('.message-input input[type="text"]');
+
+sendBtn.addEventListener('click', () => {
+  const message = messageInput.value;
+  socket.send(message);
+  // Add message to chat display
 });
+
---
+++ mariadb/mychat.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb:3306
+-- Generation Time: Mar 21, 2023 at 07:47 AM
+-- Server version: 10.6.4-MariaDB-1:10.6.4+maria~focal
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mychat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `user_id`, `message`) VALUES
+(4, 1, 'asdfa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`) VALUES
+(1, 'adfa'),
+(2, 'Roger'),
+(3, 'Samual'),
+(4, 'Johnny\r\n');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
